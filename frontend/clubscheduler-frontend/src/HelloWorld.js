f56b469aa46445fb8589d7bebed73000eeb99dde
darango3002 <donaldarango@ufl.edu>
2024-03-01 13:35:39 -0500

    changed hello world frontend

--- frontend/clubscheduler-frontend/src/HelloWorld.js
+++ frontend/clubscheduler-frontend/src/HelloWorld.js
@@ -1,23 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 function HelloWorld() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    axios.get('http://localhost:8000/hello-world/')
-      .then(response => {
-        setMessage(response.data.message);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-
   return (
     <div>
-      <h1>Should display message below if running backend:</h1>
-      <p>MESSAGE: {message}</p>
+      <h1>
+        Hello World
+      </h1>
     </div>
   );
 }
