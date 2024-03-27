85a5aae630ee7c0e29c51cba51de709a382e504e
Dcminh333 <dcminh333@gmail.com>
2024-03-27 11:37:15 -0400

    My stuff

--- frontend/clubscheduler-frontend/src/pages/ClubCreation.js
+++ frontend/clubscheduler-frontend/src/pages/ClubCreation.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import React from "react";
 import { BiUser } from 'react-icons/bi'
+import styled from 'styled-components';
 
 const ClubCreation = () => {
 
@@ -11,13 +12,12 @@ const ClubCreation = () => {
 
     })
 
-
     const {club_name, club_descript, Category } = formData
 
     return (
         <>
-            <div className="container auth__container">
-                <form className="auth__form">
+            <div className="container auth__clubcontainer">
+                <form className="auth__clubform">
                     <h2 className="main__title">Create Club <BiUser /> </h2>
 
                     <input type="text"
@@ -26,13 +26,24 @@ const ClubCreation = () => {
                         required
                     />
 
-                    <input type="text"
+                    <textarea type="text"
                         placeholder="Club Description"
                         name="club_descript"
                         required
                     />
 
+                 
+                    <h3>Club Type</h3>
+                    <select className="">
+                    <option value="1">Academic</option>
+                    <option value="2">Social</option>
+                    <option value="3">Sports</option>
+                    <option value="4">Cultural</option>
+                    <option value="5">Professional</option>
 
+                    </select> 
+
+                
                     <button className="btn btn-primary" type="submit" onClick="">Create Club</button>
                 </form>
             </div>
