fbe53e86f7e9016280850fb53ff9643f60392b10
darango3002 <donaldarango@ufl.edu>
2024-03-29 23:02:29 -0400

    Added button to ExploreClub and added to Nav Bar

--- frontend/clubscheduler-frontend/src/pages/ExploreClubs.js
+++ frontend/clubscheduler-frontend/src/pages/ExploreClubs.js
@@ -1,3 +1,24 @@
-export default function ExploreClubs(){
-    return <h1>Explore Clubs</h1>
+import { useNavigate } from "react-router-dom"
+import React from "react"
+
+export default function ExploreClubs() {
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        navigate("/create-club")
+    }
+
+    return (
+        <>
+        <div className="container">
+            <h1>Explore Clubs</h1>
+            <button className="btn btn-primary" onClick={handleClick}> 
+                Create a Club
+            </button>
+        </div>
+        
+        </>
+
+    )
+
 }
