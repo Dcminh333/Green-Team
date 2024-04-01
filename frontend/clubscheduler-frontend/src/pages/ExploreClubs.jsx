4c2c2e8eb6fa7b6a390d8ab62d5fdad976c65b61
darango3002 <donaldarango@ufl.edu>
2024-04-01 16:40:28 -0400

    changes pages file extensions to jsx

---
+++ frontend/clubscheduler-frontend/src/pages/ExploreClubs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ExploreClubs = () => {
+  return (
+    <>
+    <div className='container'>
+        <h1>Explore Clubs</h1>
+    </div>
+    </>
+  )
+}
+
+export default ExploreClubs
