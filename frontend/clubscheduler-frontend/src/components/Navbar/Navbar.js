aae8f1011a264c99440205c7544690569fe2bdf4
darango3002 <donaldarango@ufl.edu>
2024-04-15 23:06:35 -0400

    fixed navbar logout highlighting

--- frontend/clubscheduler-frontend/src/components/Navbar/Navbar.js
+++ frontend/clubscheduler-frontend/src/components/Navbar/Navbar.js
@@ -44,7 +44,12 @@ export default function Navbar()
 
 function CustomLink({ to, children, ...props }) {
     const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({path: resolvedPath.pathname, end: true})
+    let isActive = useMatch({path: resolvedPath.pathname, end: true})
+
+    if (to === "/") {
+        isActive = false;
+    }
+
     return (
         <li className={isActive ? "active" : ""}>
             <Link to={to} {...props}>{children}</Link>
